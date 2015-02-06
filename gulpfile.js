52210b43dad9f79729b9457403d64842e597cb82
Nick Tomlin <nick.tomlin@gmail.com>
2015-02-06 10:11:08 -0600

    Add http-server, comments, and other niceties

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserify = require('browserify')
 var source = require('vinyl-source-stream')
 var rename = require('gulp-rename')
 var watchify = require('watchify')
+var exit = require('gulp-exit')
 var envify = require('envify/custom')
 var merge = require('merge-stream')
 
@@ -21,43 +22,53 @@ var targets = [
   'three'
 ]
 
+// bundle our "vendored"/external bundles
+// too keep things nice and small
 var vendorBundle = browserify('./src/vendor.js')
     .require(externalBundles)
 
+// transform our source targets
+// into an object that our bundle task consumes
 var bundles = targets.map(function (target) {
   var path = './src/' + target + '/index.js'
   var bundle = watchify(browserify(path), watchify.args)
       .external(externalBundles)
+      // tack any additional browserify transforms on here
       .transform(envify())
-
   return {
     target: target,
     bundle: bundle
   }
 }).map(function (bundle, index) {
-  bundle.bundle.on('update', bundleIt.bind(null, bundle))
+  // watchify will notify us when a dep changes
+  // when that happens, we rebundle
+  bundle.bundle.on('update', bundleIt)
+  return bundle
+})
 
-  return bundle;
-});
-
-function bundleIt (obj) {
-  return obj.bundle
-    .bundle()
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source(obj.target))
-    .pipe(rename({
-      suffix: '.built',
-      extname: '.js'
-    }))
-    .pipe(gulp.dest('dist'))
+function bundleIt () {
+  return merge.apply(null, bundles.map(function (obj) {
+    return obj.bundle
+      .bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source(obj.target))
+      .pipe(rename({
+        suffix: '.built',
+        extname: '.js'
+      }))
+  }))
 }
 
-gulp.task('build', function () {
-  var built = bundles.map(function (bundle) {
-    return bundleIt(bundle)
-  })
+gulp.task('build', ['build:external'], function () {
+   return bundleIt()
+    .pipe(gulp.dest('dist'))
+    // hack to kill watchify
+    .pipe(exit())
+})
 
-  return merge.apply(null, built)
+gulp.task('build:watch', ['build:external'], function () {
+  return bundleIt()
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('build:external', function () {
@@ -67,4 +78,4 @@ gulp.task('build:external', function () {
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('default', ['build:external', 'build']);
+gulp.task('default', ['build:watch'])
